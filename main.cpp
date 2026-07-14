4f1ea24d6f7523a2eb84b57bc9b39d461b1232b0
Juan Parra <parrapradoj@gmail.com>
2026-07-14 17:23:56 -0500

    Add main function to initialize Controlador

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "Controlador.h"
+
+/*
+g++ main.cpp Controlador.cpp Mision.cpp Recurso.cpp Vehiculo.cpp Ambulancia.cpp Helicoptero.cpp Personal.cpp Medico.cpp Rescatista.cpp -o output/main 
+./output/main
+*/
+
+int main() {
+    Controlador controlador;
+    controlador.iniciar();
+    return 0;
+}
