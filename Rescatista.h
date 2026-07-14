82bea6366c9ac631b4f508268731221fd4218b70
Juan Parra <parrapradoj@gmail.com>
2026-07-14 18:30:32 -0500

    Override destructor in Rescatista class

--- Rescatista.h
+++ Rescatista.h
@@ -8,7 +8,7 @@
 class Rescatista : public Personal {
 public:
     Rescatista(const std::string& codigo, const std::string& nombre, const std::string& especialidad);
-    ~Rescatista();
+    ~Rescatista() override;
 
     void ejecutarAccion() const override;
 };
