77c969f28ab80608c8f31f46a0ae50c244359fa4
Juan Parra <parrapradoj@gmail.com>
2026-07-14 10:05:01 -0500

    Add dynamic resizing for resources and missions
    Implemented methods to resize resources and missions arrays dynamically when capacity is reached.

--- Controlador.cpp
+++ Controlador.cpp
@@ -39,3 +39,63 @@ Controlador::~Controlador() {
     std::cout << "\nMemoria liberada" << '\n';
 }
 
+
+
+/*
+En Mision.cpp hay un Método similar, pero ese borra y crea un arreglo para los "recursosAsignados"
+Eso es un atributo privado, no se debe tocar desde otra clase
+*/
+
+/*
+Duplica la capacidad del arreglo cuando se llena, copiando los punteros existentes 
+a un nuevo bloque de memoria en el Heap
+*/
+
+void Controlador::redimensionarRecursos() {
+    int nuevaCapacidad = capacidadRecursos * 2;
+    Recurso** nuevoArreglo = new Recurso*[nuevaCapacidad];
+
+    for (int i = 0; i < cantidadRecursos; i++) {
+        nuevoArreglo[i] = inventarioRecursos[i];
+    }
+
+    delete[] inventarioRecursos;
+    inventarioRecursos = nuevoArreglo;
+    capacidadRecursos = nuevaCapacidad;
+}
+
+
+
+
+void Controlador::redimensionarMisiones() {
+    int nuevaCapacidad = capacidadMisiones * 2;
+    Mision** nuevoArreglo = new Mision*[nuevaCapacidad];
+
+    for (int i = 0; i < cantidadMisiones; i++) {
+        nuevoArreglo[i] = misiones[i];
+    }
+
+    delete[] misiones;
+    misiones = nuevoArreglo;
+    capacidadMisiones = nuevaCapacidad;
+}
+
+
+
+void Controlador::agregarRecursoInventario(Recurso* recurso) {
+    if (cantidadRecursos == capacidadRecursos) {
+        redimensionarRecursos();
+    }
+    inventarioRecursos[cantidadRecursos] = recurso;
+    cantidadRecursos++;
+}
+
+
+
+void Controlador::agregarMisionSistema(Mision* mision) {
+    if (cantidadMisiones == capacidadMisiones) {
+        redimensionarMisiones();
+    }
+    misiones[cantidadMisiones] = mision;
+    cantidadMisiones++;
+}
